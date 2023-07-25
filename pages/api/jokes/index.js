13d6de285d98f4ba532d4de8ec2d38d7609ddfb4
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2023-07-25 10:22:34 +0200

    add demo code

--- pages/api/jokes/index.js
+++ pages/api/jokes/index.js
@@ -6,6 +6,16 @@ export default async function handler(request, response) {
 
   if (request.method === "GET") {
     const jokes = await Joke.find();
-    return response.status(200).json(jokes);
+    response.status(200).json(jokes);
+    return;
+  }
+
+  if (request.method === "POST") {
+    const jokeData = request.body;
+
+    await Joke.create(jokeData);
+
+    response.status(201).json({ status: "Joke created!" });
+    return;
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,41 @@
 import JokeList from "../components/JokeList";
+import useSWR from "swr";
 
 export default function HomePage() {
-  return <JokeList />;
+  const { mutate } = useSWR("/api/jokes");
+
+  async function handleCreate(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const jokeData = Object.fromEntries(formData);
+
+    console.log(jokeData);
+
+    const response = await fetch("api/jokes", {
+      method: "POST",
+      body: JSON.stringify(jokeData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+
+    const data = await response.json();
+    console.log(data);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleCreate}>
+        <label htmlFor="joke">Create a new joke</label>
+        <input type="text" id="joke" name="joke" />
+        <button type="submit">Submit</button>
+      </form>
+      <JokeList />
+    </>
+  );
 }
